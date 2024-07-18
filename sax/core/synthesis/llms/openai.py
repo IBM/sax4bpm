d97b0e6ad13a5f17f923e6d44d83f411db6e0f71
Inna Skarbovsky <INNA@il.ibm.com>
2024-07-18 17:35:22 +0300

    rename env variables to serve as private fields
    Signed-off-by: 6yuvaldavid <86296815+6yuvaldavid@users.noreply.github.com>

--- sax/core/synthesis/llms/openai.py
+++ sax/core/synthesis/llms/openai.py
@@ -10,15 +10,15 @@ from sax.core.synthesis.llms.base_llm import BaseLLM
 class OpenAILLM(BaseLLM):
     """OpenAI LLM wrapper, implementation of BaseLLM
     """
-    api_key = os.getenv("OPENAI_KEY")
+    _api_key = os.getenv("OPENAI_KEY")
 
     def __init__(self, model, temperature):  
         super().__init__(model,temperature)         
         self._model = ChatOpenAI(             
             model_name=model,
-            openai_api_key=OpenAILLM.api_key,
+            openai_api_key=OpenAILLM._api_key,
             temperature=temperature
         ) 
-        self._embeddingModel = OpenAIEmbeddings(openai_api_key=OpenAILLM.api_key) 
+        self._embeddingModel = OpenAIEmbeddings(openai_api_key=OpenAILLM._api_key) 
 
     
