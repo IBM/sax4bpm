a83ee603423c590fa63a2f67fceec8c71ff2a36d
Inna Skarbovsky <INNA@il.ibm.com>
2024-08-13 11:21:38 +0300

    update validation of environment variables

--- sax/core/synthesis/llms/openai.py
+++ sax/core/synthesis/llms/openai.py
@@ -10,15 +10,16 @@ from sax.core.synthesis.llms.base_llm import BaseLLM
 class OpenAILLM(BaseLLM):
     """OpenAI LLM wrapper, implementation of BaseLLM
     """
-    _api_key = os.getenv("OPENAI_KEY")
-
-    def __init__(self, model, temperature):  
+    def __init__(self, model, temperature):
+        self.api_key = os.getenv("OPENAI_KEY")
+        if not self.api_key:
+            raise ValueError("The API key for OpenAI is not set. Please set the OPENAI_KEY environment variable.")  
         super().__init__(model,temperature)         
         self._model = ChatOpenAI(             
             model_name=model,
-            openai_api_key=OpenAILLM._api_key,
+            openai_api_key=self.api_key,
             temperature=temperature
         ) 
-        self._embeddingModel = OpenAIEmbeddings(openai_api_key=OpenAILLM._api_key) 
+        self._embeddingModel = OpenAIEmbeddings(openai_api_key=self.api_key) 
 
     
--- sax/core/synthesis/llms/watsonx.py
+++ sax/core/synthesis/llms/watsonx.py
@@ -11,13 +11,17 @@ from sax.core.synthesis.llms.base_llm import BaseLLM
 class WatsonXLLM(BaseLLM):
     """WatsonX LLM wrapper, implementation of BaseLLM
     """
-    _genai_key=os.getenv("GENAI_KEY")
-    _genai_api=os.getenv("GENAI_API")
-
-    def __init__(self, model, temperature):
+    def __init__(self, model, temperature): 
+        self.genai_key = os.getenv("GENAI_KEY")                
+        self.genai_api= os.getenv("GENAI_API")       
+        if not self.genai_key:
+            raise ValueError("The API key for GenAI is not set. Please set the GENAI_KEY environment variable.")
+        if not self.genai_api:
+            raise ValueError("The API URL for GenAI is not set. Please set the GENAI_API environment variable.")
+        super().__init__(model,temperature) 
         
-        super().__init__(model,temperature)     
-        credentials = Credentials(api_key=WatsonXLLM._genai_key, api_endpoint=WatsonXLLM._genai_api)
-        client = Client(credentials=credentials)     
-        self._model = LangChainInterface(model_id=model, client=client)            
-        self._embeddingModel =LangChainEmbeddingsInterface(client=client, model_id="sentence-transformers/all-minilm-l6-v2",parameters=TextEmbeddingParameters(truncate_input_tokens=True))
+        credentials = Credentials(api_key=self.genai_key, api_endpoint=self.genai_api)
+        client = Client(credentials=credentials)
+        client = Client(credentials=Credentials.from_env())
+        self._model = LangChainInterface(model_id=model, client=client)                
+        self._embeddingModel =LangChainEmbeddingsInterface(client=client,model_id="sentence-transformers/all-minilm-l6-v2",parameters=TextEmbeddingParameters(truncate_input_tokens=True))
